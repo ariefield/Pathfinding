6b6a86a9070a581c83cd2b7bec55fdc3610a80b7
Arie Field <arie.field@gmail.com>
2020-06-10 02:35:52 -0400

    Refactor bfs

--- PathFinding/src/Search.cs
+++ PathFinding/src/Search.cs
@@ -16,9 +16,11 @@ namespace PathFinding.src
         public bool Searching { get; set; }
         public bool AutoAdvance { get; set; }
         public Dictionary<Tile, bool> Seen { get; set; }
+        public Dictionary<Tile, Tile> CameFrom { get; set; }
         public Tile Start { get; set; }
         public Tile Current { get; set; }
         public Tile Goal { get; set; }
+        public List<Tile> Path { get; set; }
         public int Steps { get; set; }
 
         private Queue<Tile> _queue;
@@ -32,6 +34,8 @@ namespace PathFinding.src
             Searching = false;
             AutoAdvance = false;
             Seen = new Dictionary<Tile, bool>();
+            CameFrom = new Dictionary<Tile, Tile>();
+            Path = new List<Tile>();
             Steps = 0;
 
             _queue = new Queue<Tile>();
@@ -39,12 +43,9 @@ namespace PathFinding.src
 
         public void Update()
         {
-            do
-            {
-                Current = _queue.Dequeue();
-                Steps += 1;
-            } while (Seen.ContainsKey(Current));
-
+            Steps += 1;
+            Current = _queue.Dequeue();
+            Seen[Current] = true;
 
             if (Current == Goal)
             {
@@ -54,14 +55,14 @@ namespace PathFinding.src
                 return;
             }
 
-
-            Seen[Current] = true;
-
-
             foreach (Tile tile in Current.Neighbours)
             {
-                _queue.Enqueue(tile);
+                if (!Seen.ContainsKey(tile) && !_queue.Contains(tile))
+                    _queue.Enqueue(tile);
             }
+
+            if (_queue.Count == 0)
+                Searching = false;
 
         }
 
@@ -70,6 +71,7 @@ namespace PathFinding.src
             Searching = true;
             Steps = 0;
             Seen = new Dictionary<Tile, bool>();
+            CameFrom = new Dictionary<Tile, Tile>();
             _queue = new Queue<Tile>();
 
             if (type == SearchType.Bfs)
